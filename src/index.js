17e5fd107c629a50319bcdade4f261c61a910aad
kunalavghade <kunalavghade77@gmail.com>
2023-08-08 15:05:58 +0530

    start

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
+import reportWebVitals from "./reportWebVitals";
+import StarRating from "./StartRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+    <StarRating />
   </React.StrictMode>
 );
 
